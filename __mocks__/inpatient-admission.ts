5925107992e8260f52d9ca560c9c43af2abccb23
abertnamanya <abertnamanya@gmail.com>
2025-04-16 12:22:26 +0300

    Updated package names to zimhospicare from openmrs.

--- __mocks__/inpatient-admission.ts
+++ __mocks__/inpatient-admission.ts
@@ -1,4 +1,4 @@
-import { type InpatientAdmission } from '@openmrs/esm-ward-app/src/types';
+import { type InpatientAdmission } from '@zimhospicare/esm-ward-app/src/types';
 import { mockEncounterAlice } from './encountes.mock';
 import { mockPatientAlice } from './patient.mock';
 import { mockVisitAlice } from './visits.mock';
